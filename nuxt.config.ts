9e4fa03b7a2ffe0eda517e03eab9da449b5c19b1
deribew <deribewsoftware@gmail.com>
2024-11-09 09:41:23 +0300

    deploy: project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,31 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // Set the deployment target to static
+  target: 'static', 
+
+  // Configure static site generation
+  generate: {
+    dir: 'dist',  // Output directory for static files
+  },
+
+  app: {
+    baseURL: '/aladia-test-project/', // Set the base URL for the GitHub Pages repository
+  },
+
+  // Global CSS and PostCSS configuration
   css: ['~/assets/css/main.css'],
+  
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  // Optional: Build options
+  build: {
+    // You can add custom build options here if necessary
+  }
 })
